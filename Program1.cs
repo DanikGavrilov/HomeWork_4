276df5abd048c2c1c919c4c029bd4510c3b892b7
Daniel Gavrilov <danik08242001@gmail.com>
2023-12-13 20:21:37 +0200

    first commit

---
+++ Program1.cs
@@ -0,0 +1,15 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все 
+// натуральные числа в промежутке от M до N. 
+// Использовать рекурсию, не использовать циклы.
+
+int m = 1;
+int n = 10;
+
+static string GetNumbers(int m, int n)
+{
+    if(m == n)
+        return "";
+    
+    return $"{m} " + GetNumbers(m + 1, n);
+}
+System.Console.WriteLine(GetNumbers(m, n));
---
+++ Program3.cs
@@ -0,0 +1,26 @@
+﻿// Задайте произвольный массив. Выведете его элементы, начиная с конца. 
+// Использовать рекурсию, не использовать циклы.
+
+namespace Task_03
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+            int [] array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        
+            System.Console.WriteLine(ReverseArray(array, 0));
+
+        }
+
+        static string ReverseArray(int [] array, int i)
+        {
+            if(i == array.Length) 
+                return "";
+
+
+            return ReverseArray(array, i + 1) + $" {array[i]}" ;
+        }
+    }
+}
+
